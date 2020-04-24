8518101b3695d520ae1444632ebe58a8df26b105
Shawn Martinez <52Martinezs66@gmail.com>
2020-04-24 14:00:49 -0500

    updated Week3 homework

---
+++ Week3-JS-HW/index.js
@@ -0,0 +1,7 @@
+let pizzaToppings = ["Sausage", "Pepperoni", "Jalapeno", "Onion"];
+let pizzaSize = ["Small", "Medium", "Large"];
+let pizzaCrust = ["Thin", "Stuffed", "Thick"];
+let greetCustomer = function() {
+  return `"Welcome to Pizza House, our toppings are ${pizzaToppings}."`;
+};
+console.log(greetCustomer());
