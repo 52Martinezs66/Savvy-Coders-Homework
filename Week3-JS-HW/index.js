739aa38b96b8ebb93917876be7860c6c787f0236
Shawn Martinez <52Martinezs66@gmail.com>
2020-04-27 16:27:39 -0500

    This is it we had been looking at it all weekend and my internet just came back on about an hour ago

--- Week3-JS-HW/index.js
+++ Week3-JS-HW/index.js
@@ -1,7 +1,12 @@
-let pizzaToppings = ["Sausage", "Pepperoni", "Jalapeno", "Onion"];
-let pizzaSize = ["Small", "Medium", "Large"];
-let pizzaCrust = ["Thin", "Stuffed", "Thick"];
+let pizzaToppings = ["Sausage", "Pepperoni", "Mushrooms", "Jalapeno"];
+let size = ["small", "medium", "large"];
+let crust = ["thin", "thick"];
+
 let greetCustomer = function() {
   return `"Welcome to Pizza House, our toppings are ${pizzaToppings}."`;
 };
 console.log(greetCustomer());
+
+function getPizzaOrder(size, crust, pizzaToppings) {
+  return `One ${size[1]} ${crust[2]} crust pizza with ${toppings[3]}, and ${toppings[0]}`;
+}
